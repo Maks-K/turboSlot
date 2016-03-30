672d0493782fc44b0359030c890de6caeb30727b
Maks-K <Maks.M.Kushnir@gmail.com>
2016-03-30 23:54:02 +0300

    commented out unnecessary outcome control input in engine

--- enjine.js
+++ enjine.js
@@ -7,14 +7,13 @@ background.init(stage);
 var button = new Button('resources/BTN_Spin.png', 115, 115, 873, 268)
 button.init(stage);
 
-var outcome = [Math.floor(Math.random()*firstReel.reelSet.length+1), 
+/*var outcome = [Math.floor(Math.random()*firstReel.reelSet.length+1), 
 			   Math.floor(Math.random()*secondReel.reelSet.length+1), 
-			   Math.floor(Math.random()*thirdReel.reelSet.length+1)];
-
+			   Math.floor(Math.random()*thirdReel.reelSet.length+1)];*/
 var elements = [];
 var toUpdate = [];
 for ( var j = 0; j < reels.length; j++ ){
-	var reel = new Reel( j, reels[j].reelSet, reels[j].xOffset, outcome[j]);
+	var reel = new Reel( j, reels[j].reelSet, reels[j].xOffset);
 	reel.init( stage );
 	elements.push( reel );
 	toUpdate.push( reel );
