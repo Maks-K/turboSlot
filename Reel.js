1da9c70da406fdcaa1808c54eff075ea3592ca79
Maks-K <Maks.M.Kushnir@gmail.com>
2016-03-30 23:59:35 +0300

    refactoring done, comments added

--- Reel.js
+++ Reel.js
@@ -9,7 +9,6 @@ function Reel(reelNumber, reelStrip, xOffset, stopPosition) {
     this.step = 50;
     this.stepInit = 50;
     this.stopY = 0;
-
     this.reelNumber = reelNumber;
     this.state = 'stopped';
 
@@ -20,21 +19,24 @@ function Reel(reelNumber, reelStrip, xOffset, stopPosition) {
     addListener('reelSpinStart', function (params) {
             if (params == me.reelNumber) {
                 me.state = 'moving';
+                //console.log(me.state, this.y)
             }
         }
     );
-
     addListener('reelSpinStop', function (params) {
             if (params.reelNumber == me.reelNumber) {
 
                 me.stopPosition = params.stopSym;
-                //TODO - refactor
-                if (me.stopPosition == me.reelStrip.length) {
-                    me.stopY = -(me.reelStrip.length * me.SymHeight).toFixed(0)
-                } else {
-                    me.stopY = -((me.reelStrip.length - me.stopPosition) * me.SymHeight).toFixed(0);
-                }
+                this.firstSymStopPositionY = -(me.reelStrip.length * me.SymHeight).toFixed(0);
+    			this.symStopPositionY = -((me.reelStrip.length - me.stopPosition) * me.SymHeight).toFixed(0);
 
+                //TODO - refactor / done as much as possible + comments added where needed
+                if (me.stopPosition == me.reelStrip.length) { //condition of triggering the very virst symbol on the reel
+                    me.stopY = this.firstSymStopPositionY;
+                } else {
+                    me.stopY = this.symStopPositionY;
+                }
+                //console.log('reelnumber '+me.reelNumber+'  stop position' + me.stopPosition+'  stop y ' +me.stopY)
                 me.state = 'stopping';
 
             }
@@ -55,11 +57,9 @@ function Reel(reelNumber, reelStrip, xOffset, stopPosition) {
 Reel.prototype.init = function (mainContainer) {
     console.log(this.stopPosition);
     var rootContainer = new PIXI.Container(),
-        reelSetFake,
-        reelSetReal = new ReelSet(0, this.reelStrip, this.width, this.SymHeight);
+        reelSetReal = new ReelSet(0, this.reelStrip, this.width, this.SymHeight),
+        reelSetFake = new ReelSet(-this.y, this.reelStrip, this.width, this.SymHeight);
     reelSetReal.init(rootContainer);
-
-    reelSetFake = new ReelSet(-this.y, this.reelStrip, this.width, this.SymHeight);
     reelSetFake.init(rootContainer);
 
     rootContainer.position.x = this.x;
@@ -85,23 +85,27 @@ Reel.prototype.update = function () {
 
     if (this.state == 'stopping') {
 
-
-        //this.distToSym = Math.abs(( this.y ).toFixed(0) - this.stopY);
-        //if ((this.y).toFixed(0) == this.stopY) {
-        //
-        //    fireEvent('reelSpinStopped', this.reelNumber);
-        //
-        //    return true;
-        //}
-        //if (this.distToSym < this.step) {
-        //    this.step = this.distToSym;
-        //}
+    	this.distToSym = Math.abs(( this.y ).toFixed(0) - this.stopY); //finding the distance between current y and y of needed symbol
+        
+        if ((this.y).toFixed(0) == this.stopY) {	// once the distanation symbol is reached,
+        
+            this.state = 'stopped';			//the reel is stopped
+            this.step = this.stepInit;		//the step value is restored to initial value and th function execution stops
+        
+            return true;					
+        }
+        if (this.distToSym < this.step) {	// in case the distance to needed symbol is less then the initial step,
+            this.step = this.distToSym;		// the step value is changed to be equal to this distance, for the animation not to 'pass by'
+        }
     }
-
-    if (this.y + this.step < 0) {
+    if (this.state === 'moving') {
+        console.log(this.y, this.reelNumber)
+    }
+    if (this.y + this.step < 0) { 
         this.y = this.y + this.step
     } else {
-        this.y = -this.reelStrip.length * this.SymHeight;
+        this.y = -this.reelStrip.length * this.SymHeight; //initial position, number of symbols multiplied by the symbol height
     }
     this.drawNewPosition();
 };
+ 
