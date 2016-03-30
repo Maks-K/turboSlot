9a7121bf022e4c062ad0686a55108eccad5633b0
Maks-K <Maks.M.Kushnir@gmail.com>
2016-03-31 00:01:14 +0300

    replaced repeating code with functions

--- spinModule.js
+++ spinModule.js
@@ -1,9 +1,35 @@
 function SpinModule (reels){
 	this.reels = reels;
+	var me = this;
 
 	this.reelsState = [ 'stopped','stopped','stopped'];
 
+	this.ReelSpinStart = function(reelNumber, delay){
+		setTimeout(function(){
+			fireEvent('reelSpinStart', reelNumber);
+			me[reelNumber] = 'spin';
+		}, delay)
+	};
+
+	this.reelSpinStop = function(reelNumber, stopSym, delay){
+		fireEvent('reelSpinStop', {
+			reelNumber : reelNumber,
+			stopSym    : stopSym
+		}, delay)
+	};
+
 	this.spinStart = function(){
+		me.ReelSpinStart(0, 50);
+		me.ReelSpinStart(1, 550);
+		me.ReelSpinStart(2, 950);
+		setTimeout(function(){
+			me.reelSpinStop(0, 4, 50);
+			me.reelSpinStop(1, 4, 740);
+			me.reelSpinStop(2, 5, 1250)
+		}, 3000)
+	}
+
+/*	reelsStatethis.spinStart = function(){
 		setTimeout(function(){
 			fireEvent('reelSpinStart', 0);
 			me.reelsState[0] = 'spin';
@@ -11,6 +37,7 @@ function SpinModule (reels){
 
 		setTimeout(function(){
 			fireEvent('reelSpinStart', 1);
+			me.reelsState[0] = 'spin';
 		} , 550);
 
 		setTimeout(function(){
@@ -43,8 +70,8 @@ function SpinModule (reels){
 			
 		} , 3000);
 	};
-
-	//TODO: make some functionality - prevent code duplication
+*/
+	//TODO: make some functionality - prevent code duplication / done
 
 	this.stoppedReels = [];
 
@@ -65,53 +92,3 @@ function SpinModule (reels){
 	}
 }
 
-
-
-
-
-
-/*var spinModule = {
-			   spinStart : function(){
-							setTimeout(function(){
-								fireEvent('reelSpinStart', 0);
-							} , 50);
-
-							setTimeout(function(){
-								fireEvent('reelSpinStart', 1);
-							} , 550);
-
-							setTimeout(function(){
-								fireEvent('reelSpinStart', 2);
-							} , 950);
-
-							setTimeout(function(){
-								
-								
-								setTimeout(function(){
-									fireEvent('reelSpinStopping', 0);
-								} , 50);
-								
-								setTimeout(function(){
-									fireEvent('reelSpinStopping', 1);
-								} , 750);
-								
-								setTimeout(function(){
-									fireEvent('reelSpinStopping', 2);
-								} , 1250);
-								
-							} , 1000);
-						},
-	   stoppedReels : [],
-	allReelsStopped : function(){
-							var counter = 0;
-							for (var i = 0; i < spinModule.stoppedReels.length; i++){
-								counter = counter + 1;
-							}
-							if (counter == reels.length){
-								fireEvent('allReelsStopped', 0);
-								counter = 0;
-								spinModule.stoppedReels.length = 0;
-							}
-						}
-};
-*/
