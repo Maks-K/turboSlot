ab44b8032c61ad21674bacad2d30291548428620
Maks-K <Maks.M.Kushnir@gmail.com>
2016-04-03 23:14:27 +0300

    feedbacks implemented

--- Server.js
+++ Server.js
@@ -2,18 +2,32 @@ function Server (){
     var me = this;
 
     var stopQueue = [
-
-    ];
-
-    this.onRequest = function (params) {
-
-        //SOME MAGIC
-
-        me.onResponse({
+        {
             reelStopPos : [1,2,3],
             win : 200,
             winType : 'smallWin'
-        });
+        },
+        {
+            reelStopPos : [2,3,4],
+            win : 300,
+            winType : 'mediumWin'
+        },
+        {
+            reelStopPos : [4,5,6],
+            win : 5000,
+            winType : 'bigWin'
+        }
+    ];
+    var stopQueueCounter = 0;
+    this.onRequest = function (params) {//sends the stopQueueCounter response from the queue and updates counter (to 0, when necessary)
+        //SOME MAGIC
+
+        me.onResponse(stopQueue[stopQueueCounter]);
+
+        stopQueueCounter++
+        if(stopQueueCounter >= stopQueue.length){
+            stopQueueCounter = 0;
+        }
 
     };
 
