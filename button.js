41d79cb18a32873d65000f5e675e00ac0b03dd9b
Maks-K <Maks.M.Kushnir@gmail.com>
2016-04-06 07:13:27 +0200

    tweaks

--- button.js
+++ button.js
@@ -1,8 +1,10 @@
-function Button(link, width, height, x, y){
+function Button(link1, link2, width, height, x, y){
 	var me = this;
 	this.rootContainer = null;
 	this.texture = null;
-	this.link = link;
+	this.textureNotActive = null;
+	this.link1 = link1;
+	this.link2 = link2;
 	this.width = width;
 	this.height = height;
 	this.x = x;
@@ -10,12 +12,13 @@ function Button(link, width, height, x, y){
 
 	this.init = function(mainContainer){
 		var rootContainer = new PIXI.Container(),
-			texture = new PIXI.Sprite.fromImage( me.link );
+			texture = new PIXI.Sprite.fromImage( me.link1 ),
+			textureNotActive = new PIXI.Sprite.fromImage( me.link2 );
 		rootContainer.position.x = me.x;
 		rootContainer.position.y = me.y;
-		//rootContainer.visible = false;
 		
 		texture.interactive = true;
+		texture.visible = true;
 		texture.on('mousedown', me.onSpinButtonClick);
 
 		texture.height = me.height;
@@ -23,24 +26,33 @@ function Button(link, width, height, x, y){
         texture.anchor.x = 0.5;
         texture.anchor.y = 0.5;
 
-		rootContainer.addChild(texture);
+        textureNotActive.visible = false;
+        textureNotActive.height = me.height;
+        textureNotActive.width = me.width;
+        textureNotActive.anchor.x = 0.5;
+        textureNotActive.anchor.y = 0.5;
+
+		rootContainer.addChild(texture, textureNotActive);
 		mainContainer.addChild(rootContainer);
 
 		this.rootContainer = rootContainer;
 		this.texture = texture;
+		this.textureNotActive = textureNotActive;
 	};
 
 	this.onQuickStopped = function(){
 
 		me.texture.interactive = false;
-		me.rootContainer.visible = false;
+		me.texture.visible = false;
+		me.textureNotActive.visible = true;
 
 	};
 	
 	this.onAllReelsStopped = function(){
 
 		me.texture.interactive = true;
-		me.rootContainer.visible = true;
+		me.texture.visible = true;
+		me.textureNotActive.visible = false;
 
 	};
 
