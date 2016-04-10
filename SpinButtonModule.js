c6b94cc1b06c5a45c56c95cfe20c762731d86e55
Maks-K <Maks.M.Kushnir@gmail.com>
2016-04-10 10:28:14 +0300

    Vova's refactoring

--- SpinButtonModule.js
+++ SpinButtonModule.js
@@ -1,94 +1,31 @@
-function Button(link1, link2, width, height, x, y, buttonType) {
-    var me = this;
-    this.rootContainer = null;
-    this.texture = null;
-    this.textureNotActive = null;
-    this.link1 = link1;
-    this.link2 = link2;
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;
-    this.buttonType = buttonType;
-    this.state = 'up';
-    this.enabled = true;
+function SpinButtonModule(){
+	var me = this;
+	this.spinButton = null;
 
-    this.init = function (mainContainer) {
-        var rootContainer = new PIXI.Container(),
-            texture = new PIXI.Sprite.fromImage(me.link1),
-            textureNotActive = new PIXI.Sprite.fromImage(me.link2);
-        rootContainer.position.x = me.x;
-        rootContainer.position.y = me.y;
+	this.init = function(mainStage){
+		var spinButton = new Button ('resources/BTN_Spin.png', 'resources/BTN_Spin_d.png', 115, 115, 873, 268, 'spinButton')
+		spinButton.init(stage);
 
-        rootContainer.interactive = true;
-        rootContainer.on('mousedown', me.onButtonDown);
-        rootContainer.on('mouseover', me.onButtonHover);
-        rootContainer.on('mouseout', me.onButtonUnHover);
-        rootContainer.on('mouseup', me.onButtonUp);
+		spinButton.onMouseClickCallback = function () {
+			me.onSpinButtonClick();
 
+		};
 
-        texture.anchor.x = 0.5;
-        texture.anchor.y = 0.5;
+		me.spinButton = spinButton;
+	}
 
-        textureNotActive.visible = false;
-        textureNotActive.anchor.x = 0.5;
-        textureNotActive.anchor.y = 0.5;
+	this.onSpinButtonClick = function () {
+		fireEvent('spinButtonClick');
+	};
 
-        rootContainer.addChild(texture, textureNotActive);
-        mainContainer.addChild(rootContainer);
+	this.onQuickStopped = function(){
+		me.spinButton.setState('disabled');
+	};
+	
+	this.onAllReelsStopped = function(){
+		me.spinButton.setState('up');
+	};
 
-        this.rootContainer = rootContainer;
-        this.texture = texture;
-        this.textureNotActive = textureNotActive;
-    };
-
-
-    this.onButtonDown = function () {
-        me.setState('down');//fireevent for sounds for sounds (for all states)
-
-        me.texture.scale.set(1.1);
-    };
-
-    this.onButtonUp = function () {
-        me.setState('up');
-
-        me.texture.visible = true;
-        me.textureNotActive.visible = false;
-
-        me.texture.scale.set(1);
-
-        me.onMouseClickCallback();
-    };
-
-    this.onButtonHover = function () {
-        me.setState('hover');
-
-        me.texture.alpha = 0.7;
-
-    };
-
-    this.onButtonUnHover = function () {
-        me.setState('up');
-
-        me.texture.alpha = 1;
-        me.texture.scale.set(1);
-
-    };
-
-    this.onMouseClickCallback = function(){};
-
-
-    this.update = function () {
-    };
-
-    this.setState = function (newState) {
-
-        me.state = newState;
-
-    };
-
-
-
+	addListener('quickStopped', me.onQuickStopped);
+	addListener('allReelsStopped', me.onAllReelsStopped);
 }
-
-
